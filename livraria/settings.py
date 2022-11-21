4428d56d51215f5a63ae3f4feba3057633a69a10
prijki <priscila.czki@gmail.com>
2022-11-21 08:35:56 -0300

    Python anyawhere

--- livraria/settings.py
+++ livraria/settings.py
@@ -104,15 +104,6 @@ REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
